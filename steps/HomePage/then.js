fb2aa68682b94b1a5364f84ad07920587f378e4e
Marcelo de Bittencourt <marcelo@marcelodebittencourt.com>
2021-08-17 20:03:44 -0300

    "Then" file adjust to be in the right file

--- steps/HomePage/then.js
+++
@@ -1,9 +0,0 @@
-import {When} from "@cucumber/cucumber"
-
-When(/^the user enters "(.*)" into the search bar$/, keyword => {
-    $(".gLFyf.gsfi").waitForDisplayed(5000);
-    $(".gLFyf.gsfi").click();
-    $(".gLFyf.gsfi").setValue(keyword);
-    $(".gNO89b").waitForDisplayed(5000);
-    $(".gNO89b").click();
-})
