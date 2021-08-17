d61940e9fe0bde2b5af9c7eee41f2d5fbf6ac87e
Marcelo de Bittencourt <marcelo@marcelodebittencourt.com>
2021-08-17 19:39:53 -0300

    Running ok steps

---
+++ steps/HomePage/given.js
@@ -0,0 +1,5 @@
+import {Given} from "@cucumber/cucumber";
+
+Given("a web browser is at the Google home page", () => {
+    browser.url("/");    
+});
--- wdio.conf.js
+++ wdio.conf.js
@@ -139,7 +139,11 @@ exports.config = {
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
         // <string[]> (file/dir) require files before executing features
-        require: ['./features/step-definitions/steps.js'],
+        require: [
+            "./steps/**/given.js",
+            "./steps/**/when.js",
+            "./steps/**/then.js"
+        ],
         // <boolean> show full backtrace for errors
         backtrace: false,
         // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
